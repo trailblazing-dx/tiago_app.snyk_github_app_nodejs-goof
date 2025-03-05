298f215483dc080b8f458bc5f8d629fbde3a7fd1
Tiago Ferreira <ferreiratiago@outlook.com>
2025-03-05 15:06:23 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -35,37 +35,7 @@ exports.index = function (req, res, next) {
 };
 
 exports.loginHandler = function (req, res, next) {
-	  if (validator.isEmail(req.body.username)) {
-    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
-      if (users.length > 0) {
-        const redirectPage = req.body.redirectPage
-        const session = req.session
-        const username = req.body.username
-        return adminLoginSuccess(redirectPage, session, username, res)
-      } else {
-        return res.status(401).send()
-      }
-    });
-  } else {
-    return res.status(401).send()
-  }
-	// CODE below has vulnerabilities
-  /*
-  if (validator.isEmail(req.body.username)) {
-    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
-      if (users.length > 0) {
-        const redirectPage = req.body.redirectPage
-        const session = req.session
-        const username = req.body.username
-        return adminLoginSuccess(redirectPage, session, username, res)
-      } else {
-        return res.status(401).send()
-      }
-    });
-  } else {
-    return res.status(401).send()
-  }
-  */
+ // TODO: Implement
 };
 
 function adminLoginSuccess(redirectPage, session, username, res) {
@@ -99,10 +69,8 @@ exports.admin = function (req, res, next) {
 exports.get_account_details = function(req, res, next) {
   // @TODO need to add a database call to get the profile from the database
   // and provide it to the view to display
-	/*
   const profile = {}
  	return res.render('account.hbs', profile)
-  */
 }
 
 exports.save_account_details = function(req, res, next) {
@@ -360,7 +328,7 @@ exports.chat = {
 
     const message = {
       // Default message icon. Cen be overwritten by user.
-      icon: '👋',
+      icon: '🙂',
     };
 
     _.merge(message, req.body.message, {
